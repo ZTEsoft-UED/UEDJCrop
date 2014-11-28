b1c894bd4c0504dc6faaa1f748896ce10e343049
markyun <mygood@126.com>
2014-11-29 00:24:34 +0800

    使用php给图片加中文文字
    使用php给图片加中文文字

---
+++ jCrop/showChinaText.php
@@ -0,0 +1,74 @@
+<?php  
+/* 
+param $image   图象资源 
+param size     字体大小 
+param angle    字体输出角度 
+param showX    输出位置x坐标 
+param showY    输出位置y坐标 
+param font    字体文件位置 
+param content 要在图片里显示的内容 
+*/  
+
+class showChinaText {  
+    var $text = 'haha结婚啦，北京市海淀区香格里拉酒店! '; 
+    var $font = 'D:/wamp/www/jCrop/fonts/msyhbd.ttc'; //如果没有要自己加载到相应的目录下（本地www）  
+    var $angle = 0;  
+    var $size = 15;  
+    var $showX = 100;  
+    var $showY = 160;   
+      
+    function showChinaText($showText = '') {  
+        $this->text = ! isset ( $showText ) ? $showText : $this->text;  
+        $this->show ();  
+    }  
+    function createText($instring) {  
+        $outstring = "";  
+        $max = strlen ( $instring );  
+        for($i = 0; $i < $max; $i ++) {  
+            $h = ord ( $instring [$i] );  
+            if ($h >= 160 && $i < $max - 1) {  
+                $outstring .= substr ( $instring, $i, 2 );  
+                $i ++;  
+            } else {  
+                $outstring .= $instring [$i];  
+            }  
+        }  
+        return $outstring;  
+    }  
+    function show() {  
+        //输出头内容  
+         header("Content-type: image/jpg");
+        //建立图象  
+        //$image = imagecreate(400,300);  
+        $image = imagecreatefromjpeg ( "1.jpg" ); //这里的图片，换成你的图片路径  
+        //定义颜色  
+        $red = ImageColorAllocate ( $image, 255, 0, 0 );  
+        $white = ImageColorAllocate ( $image, 255, 255, 255 );  
+        $black = ImageColorAllocate ( $image, 0, 0, 0 );  
+        //填充颜色  
+       // ImageFilledRectangle($image,0,0,200,200,$red);  
+        //显示文字  
+        $txt = $this->createText ( $this->text ); 
+        //写入文字  
+        $info = $info = getImageInfo( ROOT_PATH . $ori );
+        
+        $showX = $info['height']-20;  
+        $showY = $info['width']-20;  
+        alert( $showX); 
+        imagettftext ( $image, $this->size, $this->angle, $this->$showX, $this->$showY, $white, $this->font, $txt ); 
+       //  imageString($image,5,50,10,$txt,$white);  //参数说明：5-指文字的大小。 
+
+        //显示图形  
+        imagejpeg ( $image );  
+        imagegif ( $image, "105.jpg" );  
+     
+    }  
+}  
+?>  
+
+<?php  
+  
+//使用  
+$s = new showChinaText ();  
+   ImageDestroy ( $image );   
+?>  
